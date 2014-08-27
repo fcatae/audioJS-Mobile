dd5e57a0abbb2945e466d06dd95dc8b1c25d88d5
fcatae <fabricio.catae@microsoft.com>
2014-08-27 11:36:47 -0300

    problem: WP8 does not play sound

---
+++ AudioJS_Mobile/audio.js
@@ -0,0 +1,30 @@
+﻿function init() {
+}
+
+function log(text) {
+    var msgstatus = document.getElementById("status");
+    msgstatus.textContent = text;
+}
+
+function htmlplay() {
+    var snd = document.querySelector("audio");
+    snd.play();
+    log("playing HTML Audio");
+}
+
+function soundjsplay() {
+
+    createjs.Sound.addEventListener("fileload", function (evt) {
+        log("File loaded");
+    });
+
+    createjs.Sound.registerSound({ id: "audiobg", src: "audio/M-GameBG.mp3" });
+
+    var soundinstance = createjs.Sound.play("audio/M-GameBG.mp3");
+
+    soundinstance.addEventListener("failed", function () {
+        log("play failed");
+    });
+
+    log("playing soundJS Audio");
+}
